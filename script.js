ab440b16ae79bed59132868b5b29b777d6f7b53c
pranishch <pranishchaudhary520@gmail.com>
2025-09-02 19:35:47 +0545

    sjlkjf

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
 document.addEventListener('DOMContentLoaded', function() {
     console.log('Web app loaded');
+
+    const navbar = document.querySelector('.navbar-scroll');
+    const indicators = document.querySelector('.carousel-indicators');
+
+    window.addEventListener('scroll', () => {
+        if (window.scrollY > 50) {
+            navbar.classList.add('scrolled');
+            indicators.classList.add('hidden');
+        } else {
+            navbar.classList.remove('scrolled');
+            indicators.classList.remove('hidden');
+        }
+    });
 });
